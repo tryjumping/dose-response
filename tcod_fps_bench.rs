d276c01f9c705c4d52d8f4bccf391c381f9659a9
Tomas Sedovic <tomas@sedovic.cz>
2013-09-02 12:50:17 +0200

    Add 10 off-screen consoles in the rust bench
    Signed-off-by: Tomas Sedovic <tomas@sedovic.cz>

--- tcod_fps_bench.rs
+++ tcod_fps_bench.rs
@@ -1,4 +1,4 @@
-use std::libc::{c_int, c_char, uint8_t, c_void};
+use std::libc::{c_int, c_char, uint8_t, c_void, c_float};
 
 struct TCOD_key_t {
     vk: c_int,
@@ -56,14 +56,24 @@ extern {
     fn TCOD_console_set_char_background(con: *c_void, x: c_int, y: c_int,
                                         col: TCOD_color_t,
                                         flag: TCOD_bkgnd_flag_t) -> ();
+    fn TCOD_console_set_char_foreground(con: *c_void, x: c_int, y: c_int, col: TCOD_color_t) -> ();
     fn TCOD_console_put_char(con: *c_void, x: c_int, y: c_int, c: c_int,
                              flag: TCOD_bkgnd_flag_t) -> ();
+    fn TCOD_console_put_char_ex(con: *c_void, x: c_int, y: c_int, c: c_int,
+                                fore: TCOD_color_t, back: TCOD_color_t) -> ();
     fn TCOD_console_clear(con: *c_void) -> ();
     fn TCOD_console_flush() -> ();
     fn TCOD_console_print_ex(con: *c_void, x: c_int, y: c_int,
                              flag: TCOD_bkgnd_flag_t,
                              alignment: TCOD_alignment_t,
                              fmt: *c_char) -> ();
+    fn TCOD_console_new(w: c_int, h: c_int) -> *c_void;
+    fn TCOD_console_set_default_background(con: *c_void, col: TCOD_color_t) -> ();
+    fn TCOD_console_set_default_foreground(con: *c_void, col: TCOD_color_t) -> ();
+    fn TCOD_console_set_key_color(con: *c_void, col: TCOD_color_t) -> ();
+    fn TCOD_console_blit(src: *c_void, xSrc: c_int, ySrc: c_int, wSrc: c_int, hSrc: c_int,
+                         dst: *c_void, xDst: c_int, yDst: c_int,
+                         foreground_alpha: c_float, background_alpha: c_float) -> ();
 }
 
 fn generate_world(w: uint, h: uint) -> ~[(uint, uint, u8)] {
@@ -77,12 +87,13 @@ fn generate_world(w: uint, h: uint) -> ~[(uint, uint, u8)] {
     return result;
 }
 
-unsafe fn draw(con: *c_void, world: &~[(uint, uint, u8)], width: uint, height: uint) {
+unsafe fn draw(layers: &[*c_void], world: &~[(uint, uint, u8)], width: uint, height: uint) {
+    let con = layers[layers.len() - 1];
     for world.iter().advance |&(x, y, glyph)| {
         TCOD_console_set_char_background(con, x as c_int, y as c_int,
-                                         TCOD_color_t{r: 0, g: 0, b: 0},
-                                         TCOD_BKGND_NONE);
-        TCOD_console_put_char(con, x as c_int, y as c_int, glyph as c_int, TCOD_BKGND_NONE);
+                                         TCOD_color_t{r: 30, g: 30, b: 30},
+                                         TCOD_BKGND_SET);
+        TCOD_console_put_char(con, x as c_int, y as c_int, glyph as c_int, TCOD_BKGND_DEFAULT);
     }
     (fmt!("FPS: %?", TCOD_sys_get_fps())).as_c_str(
         |text| TCOD_console_print_ex(con, (width-1) as c_int, (height-1) as c_int,
@@ -94,25 +105,44 @@ fn main() {
     let width = 80;
     let height = 50;
     let console_count = 10;
+    let transparent_bg = TCOD_color_t{r: 255, g: 0, b: 0};
+    let white = TCOD_color_t{r: 255, g: 255, b: 255};
 
     let world = generate_world(width, height);
     let TCOD_FONT_TYPE_GREYSCALE = 4;
     let TCOD_FONT_LAYOUT_TCOD = 8;
     let TCOD_KEY_PRESSED = 1;
     let TCOD_KEY_RELEASED = 2;
-    let con = 0 as *c_void;
+    let root_console = 0 as *c_void;
+    let mut consoles: ~[*c_void] = ~[];
     unsafe {
+        for 10.times {
+            let con = TCOD_console_new(width as c_int, height as c_int);
+            TCOD_console_set_key_color(con, transparent_bg);
+            consoles.push(con);
+        }
         "./fonts/dejavu16x16_gs_tc.png".as_c_str(
             |font_path| TCOD_console_set_custom_font(font_path, TCOD_FONT_TYPE_GREYSCALE | TCOD_FONT_LAYOUT_TCOD, 32, 8));
         "tcod bench".as_c_str(|title| TCOD_console_init_root(width as c_int, height as c_int, title, 0, 2));
         while TCOD_console_is_window_closed() == 0 {
             let key = TCOD_console_check_for_keypress(TCOD_KEY_PRESSED | TCOD_KEY_RELEASED);
             if key.c == 27 { break; }
+            TCOD_console_set_default_foreground(root_console, white);
+            TCOD_console_clear(root_console);
+            for consoles.iter().advance |&con| {
+                TCOD_console_set_default_background(con, transparent_bg);
+                TCOD_console_set_default_foreground(con, white);
+                TCOD_console_clear(con);
+            }
 
-            TCOD_console_clear(con);
+            draw(consoles, &world, width, height);
 
-            draw(con, &world, width, height);
-
+            for consoles.iter().advance |&con| {
+                TCOD_console_blit(con,
+                                  0, 0, width as c_int, height as c_int,
+                                  root_console, 0, 0,
+                                  1 as c_float, 1 as c_float);
+            }
             TCOD_console_flush();
         }
     }

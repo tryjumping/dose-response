935ea63ff2cc1a23c0068ea2c16f1012f2be230b
Tomas Sedovic <tomas@sedovic.cz>
2013-10-20 14:58:18 +0200

    Give each dose a variable effect
    Signed-off-by: Tomas Sedovic <tomas@sedovic.cz>

--- src/main.rs
+++ src/main.rs
@@ -185,14 +185,14 @@ fn initial_state(width: uint, height: uint, commands: ~RingBuf<Command>,
             } else if item == world_gen::Dose {
                 e.dose = Some(c::Dose{tolerance_modifier: 1});
                 e.attribute_modifier = Some(c::AttributeModifier{
-                        state_of_mind: 40,
+                        state_of_mind: 40 + state.rng.gen_integer_range(-10, 11),
                         will: 0,
                     });
                 e.explosion_effect = Some(c::ExplosionEffect{radius: 4});
             } else if item == world_gen::StrongDose {
                 e.dose = Some(c::Dose{tolerance_modifier: 2});
                 e.attribute_modifier = Some(c::AttributeModifier{
-                        state_of_mind: 90,
+                        state_of_mind: 90 + state.rng.gen_integer_range(-15, 16),
                         will: 0,
                     });
                 e.explosion_effect = Some(c::ExplosionEffect{radius: 6});

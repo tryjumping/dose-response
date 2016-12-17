887357f2cb34fa9a6adf4c063972670108f307bb
Tomas Sedovic <tomas@sedovic.cz>
2016-12-18 00:00:23 +0100

    Add an implementation of astar
    The inner closures (including the heuristic) are still missing, but
    otherwise all the code should be there. Whether it does what it's
    supposed to is an entirely different question.

--- src/pathfinding.rs
+++ src/pathfinding.rs
@@ -1,21 +1,66 @@
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap};
+use std::usize;
+
 use point::Point;
 use level::Level;
 
-pub struct Path;
+#[derive(Debug)]
+pub struct Path {
+    path: Vec<Point>,
+}
 
 impl Path {
-    pub fn find(_from: Point, _to: Point, _level: &Level) -> Path {
+    pub fn find(_from: Point, _to: Point, _level: &Level) -> Self {
         unimplemented!()
     }
 
-    pub fn find_test(_from: Point, _to: Point, _walkability_map: &Vec<bool>) -> Path {
-        unimplemented!()
+    pub fn find_test(from: Point, to: Point, walkability_map: &Vec<bool>) -> Self {
+        let neighbors = |_current| -> Vec<Point> {
+            unimplemented!()
+        };
+
+        let cost = |_current, _next| -> usize {
+            unimplemented!()
+        };
+
+        let heuristic = |_destination, _next| -> usize {
+            unimplemented!()
+        };
+
+        let mut frontier = BinaryHeap::new();
+        frontier.push(State { position: from, cost: 0 });
+        let mut path = vec![from];
+        let mut cost_so_far = HashMap::new();
+
+        cost_so_far.insert(from, 0);
+
+        while let Some(current) = frontier.pop() {
+            if current.position == to {
+                break
+            }
+
+            for &next in neighbors(current).iter() {
+                let new_cost = cost_so_far[&current.position] + cost(current, next);
+                let val = cost_so_far.entry(next).or_insert(usize::MAX);
+                if new_cost < *val {
+                    *val = new_cost;
+                    let priority = new_cost + heuristic(to, next);
+                    frontier.push(State { position: next, cost: priority });
+                    path.push(next);
+                }
+            }
+        }
+
+        Path {
+            path: path,
+        }
     }
 
     /// The number of steps to necessary to reach the destination. If
     /// no path was found, it is `0`.
     pub fn len(&self) -> usize {
-        unimplemented!()
+        self.path.len()
     }
 }
 
@@ -23,9 +68,29 @@ impl Iterator for Path {
     type Item = Point;
 
     fn next(&mut self) -> Option<Self::Item> {
-        unimplemented!()
+        self.path.pop()
     }
 }
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct State {
+    cost: usize,
+    position: Point,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &State) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+
 
 
 #[cfg(test)]
@@ -41,6 +106,7 @@ mod test {
         Destination,
     }
 
+    #[derive(Debug)]
     struct Board {
         start: Point,
         destination: Point,
@@ -80,14 +146,16 @@ mod test {
             result.board.push(piece);
             if piece == Start {
                 assert_eq!(Point{x: -1, y: -1}, result.start);
-                result.start == Point { x: x as i32, y: y as i32 };
+                result.start = Point { x: x as i32, y: y as i32 };
             }
             if piece == Destination {
-                assert_eq!(Point{x: -1, y: -1}, result.start);
-                result.destination == Point { x: x as i32, y: y as i32 };
+                assert_eq!(Point{x: -1, y: -1}, result.destination);
+                result.destination = Point { x: x as i32, y: y as i32 };
             }
             x += 1;
         }
+        assert!(result.start != Point { x: -1, y: -1});
+        assert!(result.destination != Point { x: -1, y: -1});
         result
     }
 
@@ -105,6 +173,7 @@ mod test {
 ...........
 ");
         let path: Path = find_path(&board);
+        println!("{:?}", path);
         assert_eq!(7, path.len());
         //assert_eq!(board.path, path.collect::<Vec<_>>());
     }

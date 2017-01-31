515a3baf4676962034591db1977432d3efd7cfd5
Tomas Sedovic <tomas@sedovic.cz>
2017-01-31 19:58:31 +0100

    Implement keys

--- src/engine/glium.rs
+++ src/engine/glium.rs
@@ -5,7 +5,7 @@ use glium::{self, DisplayBuild, Surface};
 use glium::draw_parameters::DrawParameters;
 use glium::glutin::{Event, WindowBuilder};
 use glium::glutin::ElementState as PressState;
-use glium::glutin::VirtualKeyCode as GliumKey;
+use glium::glutin::VirtualKeyCode as BackendKey;
 use image;
 use image::GenericImage;
 
@@ -19,6 +19,83 @@ fn gl_color(color: Color, alpha: f32) -> [f32; 4] {
     [color.r as f32, color.g as f32, color.b as f32, alpha]
 }
 
+
+fn key_code_from_backend(backend_code: BackendKey) -> Option<KeyCode> {
+    match backend_code {
+        BackendKey::Return => Some(KeyCode::Enter),
+        BackendKey::Escape => Some(KeyCode::Esc),
+        BackendKey::Space => Some(KeyCode::Space),
+
+        BackendKey::Key0 => Some(KeyCode::D0),
+        BackendKey::Key1 => Some(KeyCode::D1),
+        BackendKey::Key2 => Some(KeyCode::D2),
+        BackendKey::Key3 => Some(KeyCode::D3),
+        BackendKey::Key4 => Some(KeyCode::D4),
+        BackendKey::Key5 => Some(KeyCode::D5),
+        BackendKey::Key6 => Some(KeyCode::D6),
+        BackendKey::Key7 => Some(KeyCode::D7),
+        BackendKey::Key8 => Some(KeyCode::D8),
+        BackendKey::Key9 => Some(KeyCode::D9),
+
+        BackendKey::A => Some(KeyCode::A),
+        BackendKey::B => Some(KeyCode::B),
+        BackendKey::C => Some(KeyCode::C),
+        BackendKey::D => Some(KeyCode::D),
+        BackendKey::E => Some(KeyCode::E),
+        BackendKey::F => Some(KeyCode::F),
+        BackendKey::G => Some(KeyCode::G),
+        BackendKey::H => Some(KeyCode::H),
+        BackendKey::I => Some(KeyCode::I),
+        BackendKey::J => Some(KeyCode::J),
+        BackendKey::K => Some(KeyCode::K),
+        BackendKey::L => Some(KeyCode::L),
+        BackendKey::M => Some(KeyCode::M),
+        BackendKey::N => Some(KeyCode::N),
+        BackendKey::O => Some(KeyCode::O),
+        BackendKey::P => Some(KeyCode::P),
+        BackendKey::Q => Some(KeyCode::Q),
+        BackendKey::R => Some(KeyCode::R),
+        BackendKey::S => Some(KeyCode::S),
+        BackendKey::T => Some(KeyCode::T),
+        BackendKey::U => Some(KeyCode::U),
+        BackendKey::V => Some(KeyCode::V),
+        BackendKey::W => Some(KeyCode::W),
+        BackendKey::X => Some(KeyCode::X),
+        BackendKey::Y => Some(KeyCode::Y),
+        BackendKey::Z => Some(KeyCode::Z),
+
+        BackendKey::F1 => Some(KeyCode::F1),
+        BackendKey::F2 => Some(KeyCode::F2),
+        BackendKey::F3 => Some(KeyCode::F3),
+        BackendKey::F4 => Some(KeyCode::F4),
+        BackendKey::F5 => Some(KeyCode::F5),
+        BackendKey::F6 => Some(KeyCode::F6),
+        BackendKey::F7 => Some(KeyCode::F7),
+        BackendKey::F8 => Some(KeyCode::F8),
+        BackendKey::F9 => Some(KeyCode::F9),
+        BackendKey::F10 => Some(KeyCode::F10),
+        BackendKey::F11 => Some(KeyCode::F11),
+        BackendKey::F12 => Some(KeyCode::F12),
+
+        BackendKey::Right => Some(KeyCode::Right),
+        BackendKey::Left => Some(KeyCode::Left),
+        BackendKey::Down => Some(KeyCode::Down),
+        BackendKey::Up => Some(KeyCode::Up),
+
+        BackendKey::Numpad1 => Some(KeyCode::NumPad1),
+        BackendKey::Numpad2 => Some(KeyCode::NumPad2),
+        BackendKey::Numpad3 => Some(KeyCode::NumPad3),
+        BackendKey::Numpad4 => Some(KeyCode::NumPad4),
+        BackendKey::Numpad5 => Some(KeyCode::NumPad5),
+        BackendKey::Numpad6 => Some(KeyCode::NumPad6),
+        BackendKey::Numpad7 => Some(KeyCode::NumPad7),
+        BackendKey::Numpad8 => Some(KeyCode::NumPad8),
+        BackendKey::Numpad9 => Some(KeyCode::NumPad9),
+        BackendKey::Numpad0 => Some(KeyCode::NumPad0),
+
+        _ => None,
+    }
+}
 
 fn texture_coords_from_char(chr: char) -> (f32, f32) {
     let (x, y) = match chr {
@@ -206,6 +283,12 @@ pub fn main_loop<T>(display_size: Point,
         fullscreen: false,
     };
     let mut drawcalls = Vec::with_capacity(4000);
+    let mut lctrl_pressed = false;
+    let mut rctrl_pressed = false;
+    let mut lalt_pressed = false;
+    let mut ralt_pressed = false;
+    let mut lshift_pressed = false;
+    let mut rshift_pressed = false;
     let mut vertices = Vec::with_capacity(drawcalls.len() * 6);
     let mut keys = vec![];
     let mut alpha = 1.0;
@@ -265,6 +348,30 @@ pub fn main_loop<T>(display_size: Point,
                 }
 
                 &Draw::Background(pos, background_color) => {
+                    let (pos_x, pos_y) = (pos.x as f32, pos.y as f32);
+                    let (tilemap_x, tilemap_y) = (0.0, 5.0);
+                    let color = gl_color(background_color, alpha);
+
+                    vertices.push(Vertex { tile_position: [pos_x,   pos_y],
+                                           tilemap_index:  [tilemap_x, tilemap_y],
+                                           color: color  });
+                    vertices.push(Vertex { tile_position: [pos_x + 1.0,   pos_y],
+                                           tilemap_index:  [tilemap_x + 1.0, tilemap_y],
+                                           color: color });
+                    vertices.push(Vertex { tile_position: [pos_x,   pos_y + 1.0],
+                                           tilemap_index:  [tilemap_x, tilemap_y + 1.0],
+                                           color: color });
+
+                    vertices.push(Vertex { tile_position: [pos_x + 1.0,   pos_y],
+                                           tilemap_index:  [tilemap_x + 1.0, tilemap_y],
+                                           color: color });
+                    vertices.push(Vertex { tile_position: [pos_x,   pos_y + 1.0],
+                                           tilemap_index:  [tilemap_x, tilemap_y + 1.0],
+                                           color: color });
+                    vertices.push(Vertex { tile_position: [pos_x + 1.0,   pos_y + 1.0],
+                                           tilemap_index:  [tilemap_x + 1.0, tilemap_y + 1.0],
+                                           color: color });
+
                 }
 
                 &Draw::Text(start_pos, ref text, color) => {
@@ -286,7 +393,8 @@ pub fn main_loop<T>(display_size: Point,
 
         // Render
         let mut target = display.draw();
-        target.clear_color(1.0, 0.0, 1.0, 1.0);
+        //target.clear_color(1.0, 0.0, 1.0, 1.0);
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
         target.draw(&vertex_buffer,
                     &glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList),
                     &program,
@@ -302,9 +410,55 @@ pub fn main_loop<T>(display_size: Point,
         for ev in display.poll_events() {
             match ev {
                 Event::Closed => return,   // the window has been closed by the user
-                Event::KeyboardInput(press_state, _, key_code) => {
-                    if let Some(code) = key_code {
-                        //self.key_events.push((code, press_state));
+
+
+                Event::KeyboardInput(PressState::Pressed, _, Some(BackendKey::LControl)) => {
+                    lctrl_pressed = true;
+                }
+                Event::KeyboardInput(PressState::Pressed, _, Some(BackendKey::RControl)) => {
+                    rctrl_pressed = true;
+                }
+                Event::KeyboardInput(PressState::Pressed, _, Some(BackendKey::LAlt)) => {
+                    lalt_pressed = true;
+                }
+                Event::KeyboardInput(PressState::Pressed, _, Some(BackendKey::RAlt)) => {
+                    ralt_pressed = true;
+                }
+                Event::KeyboardInput(PressState::Pressed, _, Some(BackendKey::LShift)) => {
+                    lshift_pressed = true;
+                }
+                Event::KeyboardInput(PressState::Pressed, _, Some(BackendKey::RShift)) => {
+                    rshift_pressed = true;
+                }
+
+                Event::KeyboardInput(PressState::Released, _, Some(BackendKey::LControl)) => {
+                    lctrl_pressed = false;
+                }
+                Event::KeyboardInput(PressState::Released, _, Some(BackendKey::RControl)) => {
+                    rctrl_pressed = false;
+                }
+                Event::KeyboardInput(PressState::Released, _, Some(BackendKey::LAlt)) => {
+                    lalt_pressed = false;
+                }
+                Event::KeyboardInput(PressState::Released, _, Some(BackendKey::RAlt)) => {
+                    ralt_pressed = false;
+                }
+                Event::KeyboardInput(PressState::Released, _, Some(BackendKey::LShift)) => {
+                    lshift_pressed = false;
+                }
+                Event::KeyboardInput(PressState::Released, _, Some(BackendKey::RShift)) => {
+                    rshift_pressed = false;
+                }
+
+                Event::KeyboardInput(PressState::Pressed, _, Some(key_code)) => {
+                    //self.key_events.push((code, press_state));
+                    if let Some(code) = key_code_from_backend(key_code) {
+                        keys.push(Key {
+                            code: code,
+                            alt: lalt_pressed || ralt_pressed,
+                            ctrl: lctrl_pressed || rctrl_pressed,
+                            shift: lshift_pressed || rshift_pressed,
+                        });
                     }
                 }
                 _ => ()

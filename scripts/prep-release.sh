dd247582e15a83c7b9692b866dbf0a7c9094b49d
Tomas Sedovic <tomas@sedovic.cz>
2017-02-24 23:13:59 +0100

    Update the build script

--- scripts/prep-release.sh
+++ scripts/prep-release.sh
@@ -4,6 +4,5 @@ set -eux
 
 VERSION=$(grep '^version\s\+' Cargo.toml | awk '{gsub(/"/, "", $3); print $3}')
 
-git commit -m "Release version v${VERSION}"
 git tag -a -m "Release version v${VERSION}" "v${VERSION}"
 git push --follow-tags github master

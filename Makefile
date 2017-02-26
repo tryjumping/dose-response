d780de51e0ad9ebcbb94f93827fbf830d2835378
Tomas Sedovic <tomas@sedovic.cz>
2017-02-26 10:42:30 +0100

    Make the release script more robust
    `make release` now makes sure the repository is not empty and that the
    release can be built locally before tagging and pushing it to github.

--- Makefile
+++ Makefile
@@ -1,24 +1,7 @@
-SOURCES=$(wildcard src/**/*.rs src/*.rs)
-
-all: build
-
-build:
-	cargo build
-
-test:
-	cargo test
-
-release:
-	cargo build --release
-
-run:
-	cargo run
-
 replay:
 	cargo run -- `find replays -type f -name 'replay-*' | sort | tail -n 1`
 
-clean:
-	rm -rf target
-
-prep-release:
+release:
 	scripts/prep-release.sh
+
+.PHONY: replay release
--- scripts/prep-release.sh
+++ scripts/prep-release.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+die() { echo "$@" 1>&2 ; exit 1; }
+
+git diff-files --quiet || die "The repo is dirty. Commint everything before pushing a release."
+
 set -eux
 
 VERSION=$(grep '^version\s\+' Cargo.toml | awk '{gsub(/"/, "", $3); print $3}')
 
+cargo build --release
 git tag -a -m "Release version v${VERSION}" "v${VERSION}"
 git push --follow-tags github master

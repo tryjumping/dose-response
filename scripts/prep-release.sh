41c06192a093d838c1b8828ff1bfa13e7d0068ef
Tomas Sedovic <tomas@sedovic.cz>
2017-03-19 11:14:20 +0100

    Update the release script
    This fixes a typo, updates the release remote to `origin` and runs the
    "check the repo is clean" after running `cargo build` as well, to pick
    up on things such as the automatic version bump in `Cargo.lock`

--- scripts/prep-release.sh
+++ scripts/prep-release.sh
@@ -2,12 +2,17 @@
 
 die() { echo "$@" 1>&2 ; exit 1; }
 
-git diff-files --quiet || die "The repo is dirty. Commint everything before pushing a release."
+verify_repo_is_clean() {
+    git diff-files --quiet || die "The repo is dirty. Commit everything before pushing a release.";
+}
 
 set -eux
+
+verify_repo_is_clean
 
 VERSION=$(grep '^version\s\+' Cargo.toml | awk '{gsub(/"/, "", $3); print $3}')
 
 cargo build --release
+verify_repo_is_clean
 git tag -a -m "Release version v${VERSION}" "v${VERSION}"
-git push --follow-tags github master
+git push --follow-tags origin master

48a01cb8b7b5f0f786f6514f889d8935891fb484
Tomas Sedovic <tomas@sedovic.cz>
2017-04-16 17:15:52 +0200

    Format main.rs

--- src/main.rs
+++ src/main.rs
@@ -54,93 +54,114 @@ mod timer;
 mod world;
 
 
+#[cfg(feature = "libtcod")]
+fn run_libtcod(display_size: point::Point,
+               default_background: color::Color,
+               window_title: &str,
+               font_path: &Path,
+               state: State) {
+    println!("Using the libtcod backend.");
+    let mut engine =
+        engine::tcod::Engine::new(display_size, default_background, window_title, &font_path);
+    engine.main_loop(state, update);
+}
+
+#[cfg(not(feature = "libtcod"))]
+fn run_libtcod(_display_size: point::Point,
+               _default_background: color::Color,
+               _window_title: &str,
+               _font_path: &Path,
+               _state: State) {
+    println!("The \"libtcod\" feature was not compiled in.");
+}
+
+#[cfg(feature = "piston")]
+fn run_piston(display_size: point::Point,
+              default_background: color::Color,
+              window_title: &str,
+              font_path: &Path,
+              state: State,
+              update: engine::UpdateFn<State>) {
+    println!("Using the piston backend.");
+    engine::piston::main_loop(display_size,
+                              default_background,
+                              window_title,
+                              &font_path,
+                              state,
+                              update);
+}
+
+#[cfg(not(feature = "piston"))]
+fn run_piston(_display_size: point::Point,
+              _default_background: color::Color,
+              _window_title: &str,
+              _font_path: &Path,
+              _state: State,
+              _update: engine::UpdateFn<State>) {
+    println!("The \"piston\" feature was not compiled in.");
+}
+
+#[cfg(feature = "terminal")]
+fn run_terminal() {
+    println!("Using the rustbox backend.\n  TODO: this is not implemented yet.");
+}
+
+#[cfg(not(feature = "terminal"))]
+fn run_terminal() {
+    println!("The \"terminal\" feature was not compiled in.");
+}
+
+#[cfg(feature = "opengl")]
+fn run_opengl(display_size: point::Point,
+              default_background: color::Color,
+              window_title: &str,
+              state: State,
+              update: engine::UpdateFn<State>) {
+    println!("Using the default backend: opengl");
+    engine::glium::main_loop(display_size,
+                             default_background,
+                             window_title,
+                             state,
+                             update);
+}
+
+#[cfg(not(feature = "opengl"))]
+fn run_opengl(_display_size: point::Point,
+              _default_background: color::Color,
+              _window_title: &str,
+              _state: State,
+              _update: engine::UpdateFn<State>) {
+    println!("The \"opengl\" feature was not compiled in.");
+}
+
+#[cfg(feature = "remote")]
+fn run_remote(display_size: point::Point,
+              default_background: color::Color,
+              window_title: &str,
+              state: State,
+              update: engine::UpdateFn<State>) {
+    engine::remote::main_loop(display_size,
+                              default_background,
+                              window_title,
+                              state,
+                              update);
+}
+
+#[cfg(not(feature = "remote"))]
+fn run_remote(_display_size: point::Point,
+              _default_background: color::Color,
+              _window_title: &str,
+              _state: State,
+              _update: engine::UpdateFn<State>) {
+    println!("The \"remote\" feature was not compiled in.");
+}
+
+
 fn main() {
     use clap::{Arg, ArgGroup, App};
 
-    #[cfg(feature = "libtcod")]
-    fn run_libtcod(display_size: point::Point, default_background: color::Color,
-                   window_title: &str, font_path: &Path,
-                   state: State) {
-        println!("Using the libtcod backend.");
-        let mut engine = engine::tcod::Engine::new(display_size, default_background, window_title, &font_path);
-        engine.main_loop(state, update);
-    }
-    #[cfg(not(feature = "libtcod"))]
-    fn run_libtcod(_display_size: point::Point, _default_background: color::Color,
-                   _window_title: &str, _font_path: &Path,
-                   _state: State) {
-        println!("The \"libtcod\" feature was not compiled in.");
-    }
-
-    #[cfg(feature = "piston")]
-    fn run_piston(display_size: point::Point,
-                  default_background: color::Color,
-                  window_title: &str,
-                  font_path: &Path,
-                  state: State,
-                  update: engine::UpdateFn<State>) {
-        println!("Using the piston backend.");
-        engine::piston::main_loop(display_size, default_background, window_title, &font_path,
-                                  state, update);
-    }
-    #[cfg(not(feature = "piston"))]
-    fn run_piston(_display_size: point::Point,
-                  _default_background: color::Color,
-                  _window_title: &str,
-                  _font_path: &Path,
-                  _state: State,
-                  _update: engine::UpdateFn<State>) {
-        println!("The \"piston\" feature was not compiled in.");
-    }
-
-    #[cfg(feature = "terminal")]
-    fn run_terminal() {
-        println!("Using the rustbox backend.\n  TODO: this is not implemented yet.");
-    }
-    #[cfg(not(feature = "terminal"))]
-    fn run_terminal() {
-        println!("The \"terminal\" feature was not compiled in.");
-    }
-
-    #[cfg(feature = "opengl")]
-    fn run_opengl(display_size: point::Point,
-                  default_background: color::Color,
-                  window_title: &str,
-                  state: State,
-                  update: engine::UpdateFn<State>) {
-        println!("Using the default backend: opengl");
-        engine::glium::main_loop(display_size, default_background, window_title,
-                                 state, update);
-    }
-    #[cfg(not(feature = "opengl"))]
-    fn run_opengl(_display_size: point::Point,
-                  _default_background: color::Color,
-                  _window_title: &str,
-                  _state: State,
-                  _update: engine::UpdateFn<State>) {
-        println!("The \"opengl\" feature was not compiled in.");
-    }
-
-    #[cfg(feature = "remote")]
-    fn run_remote(display_size: point::Point,
-                  default_background: color::Color,
-                  window_title: &str,
-                  state: State,
-                  update: engine::UpdateFn<State>) {
-        engine::remote::main_loop(display_size, default_background, window_title,
-                                  state, update);
-    }
-    #[cfg(not(feature = "remote"))]
-    fn run_remote(_display_size: point::Point,
-                  _default_background: color::Color,
-                  _window_title: &str,
-                  _state: State,
-                  _update: engine::UpdateFn<State>) {
-        println!("The \"remote\" feature was not compiled in.");
-    }
-
-    // Note: at our current font, the height of 43 is the maximum value for
-    // 1336x768 monitors.
+    // NOTE: at our current font, the height of 43 is the maximum
+    // value for 1336x768 monitors.
     let map_size = 43;
     let panel_width = 20;
     let display_size = (map_size + panel_width, map_size).into();
@@ -193,11 +214,14 @@ fn main() {
             panic!("The `replay-file` option can only be used during regular game, not replay.");
         }
         let replay_path = Path::new(replay);
-        State::replay_game(world_size, map_size, panel_width, display_size,
-                               &replay_path,
-                               matches.is_present("invincible"),
-                               matches.is_present("replay-full-speed"),
-                               matches.is_present("exit-after"))
+        State::replay_game(world_size,
+                           map_size,
+                           panel_width,
+                           display_size,
+                           &replay_path,
+                           matches.is_present("invincible"),
+                           matches.is_present("replay-full-speed"),
+                           matches.is_present("exit-after"))
     } else {
         if matches.is_present("replay-full-speed") {
             panic!("The `full-replay-speed` option can only be used if the replay log is passed.");
@@ -206,15 +230,28 @@ fn main() {
             Some(file) => Path::new(file).into(),
             None => state::generate_replay_path(),
         };
-        State::new_game(world_size, map_size, panel_width, display_size, matches.is_present("exit-after"), &replay_file, matches.is_present("invincible"))
+        State::new_game(world_size,
+                        map_size,
+                        panel_width,
+                        display_size,
+                        matches.is_present("exit-after"),
+                        &replay_file,
+                        matches.is_present("invincible"))
     };
 
-    if  matches.is_present("libtcod") {
-        run_libtcod(display_size, color::background, title, &Path::new(""), state);
-    }
-    else if matches.is_present("piston") {
-        run_piston(display_size, color::background, title, &Path::new(""),
-                   state, game::update);
+    if matches.is_present("libtcod") {
+        run_libtcod(display_size,
+                    color::background,
+                    title,
+                    &Path::new(""),
+                    state);
+    } else if matches.is_present("piston") {
+        run_piston(display_size,
+                   color::background,
+                   title,
+                   &Path::new(""),
+                   state,
+                   game::update);
     } else if matches.is_present("terminal") {
         run_terminal();
     } else if matches.is_present("remote") {
@@ -222,5 +259,4 @@ fn main() {
     } else {
         run_opengl(display_size, color::background, title, state, game::update);
     }
-
 }

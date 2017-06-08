1e497c0c31bbf42ce05aba258a2dfeba2657880c
Tomas Sedovic <tomas@sedovic.cz>
2017-06-08 16:44:21 +0200

    Add flags to indicate blocking features
    These will replace the `Walkability` enum.

--- src/level.rs
+++ src/level.rs
@@ -78,6 +78,14 @@ pub enum Walkability {
     BlockingMonsters,
 }
 
+bitflags! {
+    /// Flag to indicate features that block pathfinding/walking.
+    pub struct Blocker: u32 {
+        const WALL    = 0b00000001;
+        const MONSTER = 0b00000010;
+        const PLAYER  = 0b00000100;
+    }
+}
 
 pub struct Level {
     dimensions: point::Point,

f8acebc2c47cf3e37126d30436d41e4a68619228
Tomas Sedovic <tomas@sedovic.cz>
2017-12-20 16:59:12 +0100

    Pull drawcall sorting+dedup out into a function
    We can use it from the wasm backend, too.

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -5,6 +5,7 @@ use point::Point;
 use state::State;
 use std::borrow::Cow;
 
+use std::ops::RangeFrom;
 use std::time::Duration;
 
 
@@ -33,6 +34,19 @@ pub enum Draw {
     Fade(f32, Color),
 }
 
+
+// NOTE: This is designed specifically to deduplicated characters on
+// the same position (using Vec::dedup). So the only thing considered
+// equal are characters with the same pos value.
+impl PartialEq for Draw {
+    fn eq(&self, other: &Self) -> bool {
+        use engine::Draw::*;
+        match (self, other) {
+            (&Char(p1, ..), &Char(p2, ..)) => p1 == p2,
+            _ => false,
+        }
+    }
+}
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Mouse {
@@ -73,6 +87,53 @@ pub type UpdateFn = fn(&mut State,
                        settings: &mut Settings,
                        drawcalls: &mut Vec<Draw>)
                        -> RunningState;
+
+
+/// Sort the drawcalls in the specified range. Then "reverse
+/// deduplicate" them -- meaning the latest item stays (rather than
+/// the first one in a normal dedup).
+pub fn sort_drawcalls(drawcalls: &mut Vec<Draw>, range: RangeFrom<usize>) {
+    use std::cmp::Ordering::*;
+    use engine::Draw::*;
+    drawcalls[range].sort_by(|a, b| {
+        match (a, b) {
+            (&Char(p1, ..), &Char(p2, ..)) => {
+                let x_ordering = p1.x.cmp(&p2.x);
+                if x_ordering == Equal {
+                    p1.y.cmp(&p2.y)
+                } else {
+                    x_ordering
+                }
+            }
+
+            (&Background(..), &Background(..)) => Equal,
+            (&Text(..), &Text(..)) => Equal,
+            (&Rectangle(..), &Rectangle(..)) => Equal,
+            (&Fade(..), &Fade(..)) => Equal,
+
+            (&Fade(..), _) => Greater,
+            (_, &Fade(..)) => Less,
+
+            (&Background(..), &Char(..)) => Less,
+            (&Char(..), &Background(..)) => Greater,
+
+            (&Background(..), &Text(..)) => Less,
+            (&Text(..), &Background(..)) => Greater,
+
+            (&Background(..), &Rectangle(..)) => Less,
+            (&Rectangle(..), &Background(..)) => Greater,
+
+            _ => Equal,
+        }
+    });
+
+    // Remove duplicate background and foreground tiles. I.e. for
+    // any given point, only the last specified drawcall of the
+    // same kind will remain.
+    drawcalls.reverse();
+    drawcalls.dedup();
+    drawcalls.reverse();
+}
 
 
 // NOTE:

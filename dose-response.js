57cb078571c2e654cd7dde8b94ebbe78139fca3e
Tomas Sedovic <tomas@sedovic.cz>
2017-12-28 01:00:20 +0100

    Isolate the Dose Response JS into one function
    That way we can control its execution say by showing a "click here to
    play" banner or whatever.
    
    We also don't pollute the global namespace as much and we take the
    path to the wasm file as a parameter now which means it can be driven
    externally as well.
    
    As a benefit to this, releasing a new build on the project website is
    just a matter of copying the appropriate *.js and *.css files over.

--- dose-response.js
+++ dose-response.js
@@ -11,106 +11,107 @@ var ctx = c.getContext('2d');
 ctx.textAlign = "center";
 ctx.font = '14px mononoki';
 
-var wasm_instance;
-var gamestate_ptr;
-var pressed_keys = [];
+function play_game(wasm_path) {
+  var wasm_instance;
+  var gamestate_ptr;
+  var pressed_keys = [];
 
+  fetch(wasm_path)
+    .then(response => response.arrayBuffer())
 
-fetch('target/wasm32-unknown-unknown/release/dose-response.wasm')
-  .then(response => response.arrayBuffer())
+    .then(bytes => WebAssembly.instantiate(bytes, {
+      env: {
+        random: Math.random,
+        draw: function(ptr, len) {
+          if(len % 6 != 0) {
+            throw new Error("The drawcalls vector must have a multiple of 6 elements!");
+          }
 
-  .then(bytes => WebAssembly.instantiate(bytes, {
-    env: {
-      random: Math.random,
-      draw: function(ptr, len) {
-        if(len % 6 != 0) {
-          throw new Error("The drawcalls vector must have a multiple of 6 elements!");
+          memory = new Uint8Array(wasm_instance.exports.memory.buffer, ptr, len);
+
+          ctx.clearRect(0, 0, width * squareSize, height * squareSize);
+
+          for(let i = 0; i < len; i += 6) {
+            let x = memory[i + 0];
+            let y = memory[i + 1];
+            var glyph = null;
+            if(memory[i + 2] != 0) {
+              glyph = String.fromCharCode(memory[i + 2]);
+            }
+            let r = memory[i + 3];
+            let g = memory[i + 4];
+            let b = memory[i + 5];
+
+            // NOTE: (255, 255) position means fade
+            if(x == 255 && y == 255) {
+              // NOTE: alpha is stored in the glyph position
+              let alpha = memory[i + 2] / 255;  // convert the "alpha" to <0, 1>
+              ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${alpha})`;
+              ctx.fillRect(0, 0, width * squareSize, height * squareSize);
+            } else if(glyph === null) {
+              ctx.fillStyle = `rgb(${r},${g},${b})`;
+              ctx.fillRect(x * squareSize, y * squareSize, squareSize, squareSize);
+            } else {
+              ctx.fillStyle = `rgb(${r},${g},${b})`;
+
+              let x_fudge = 8;
+              let y_fudge = 13;
+              ctx.fillText(glyph, x * squareSize + x_fudge, y * squareSize + y_fudge);
+            }
+          }
+
+        }
+      }
+    }))
+
+    .then(results => {
+
+      document.addEventListener('keydown', (event) => {
+        let key = normalize_key(event);
+
+        // Prevent default for these keys. They will scroll the page
+        // otherwise.
+        let stopkeys = {
+          "ArrowDown": true,
+          "ArrowUp": true,
+          "ArrowLeft": true,
+          "ArrowRight": true,
+          " ": true,
+          "PageDown": true,
+          "PageUp": true,
+          "Home": true,
+          "End": true
+        };
+        if(stopkeys[key.name]) {
+          event.preventDefault();
         }
 
-        memory = new Uint8Array(wasm_instance.exports.memory.buffer, ptr, len);
-
-        ctx.clearRect(0, 0, width * squareSize, height * squareSize);
-
-        for(let i = 0; i < len; i += 6) {
-          let x = memory[i + 0];
-          let y = memory[i + 1];
-          var glyph = null;
-          if(memory[i + 2] != 0) {
-            glyph = String.fromCharCode(memory[i + 2]);
-          }
-          let r = memory[i + 3];
-          let g = memory[i + 4];
-          let b = memory[i + 5];
-
-          // NOTE: (255, 255) position means fade
-          if(x == 255 && y == 255) {
-            // NOTE: alpha is stored in the glyph position
-            let alpha = memory[i + 2] / 255;  // convert the "alpha" to <0, 1>
-            ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${alpha})`;
-            ctx.fillRect(0, 0, width * squareSize, height * squareSize);
-          } else if(glyph === null) {
-            ctx.fillStyle = `rgb(${r},${g},${b})`;
-            ctx.fillRect(x * squareSize, y * squareSize, squareSize, squareSize);
-          } else {
-            ctx.fillStyle = `rgb(${r},${g},${b})`;
-
-            let x_fudge = 8;
-            let y_fudge = 13;
-            ctx.fillText(glyph, x * squareSize + x_fudge, y * squareSize + y_fudge);
-          }
+        if(key.numerical_code != 0) {
+          pressed_keys.push(key);
         }
+      }, true);
 
+      wasm_instance = results.instance;
+      gamestate_ptr = results.instance.exports.initialise();
+
+      var previous_frame_timestamp = 0;
+
+      function update(timestamp) {
+        window.requestAnimationFrame(update);
+        let dt = timestamp - previous_frame_timestamp;
+        previous_frame_timestamp = timestamp;
+
+        for(let key of pressed_keys) {
+          wasm_instance.exports.key_pressed(
+            gamestate_ptr,
+            key.numerical_code,
+            key.ctrl, key.alt, key.shift);
+        }
+        pressed_keys = [];
+
+        results.instance.exports.update(gamestate_ptr, dt);
       }
-    }
-  }))
+      update(previous_frame_timestamp);
 
-  .then(results => {
-
-    document.addEventListener('keydown', (event) => {
-      let key = normalize_key(event);
-
-      // Prevent default for these keys. They will scroll the page
-      // otherwise.
-      let stopkeys = {
-        "ArrowDown": true,
-        "ArrowUp": true,
-        "ArrowLeft": true,
-        "ArrowRight": true,
-        " ": true,
-        "PageDown": true,
-        "PageUp": true,
-        "Home": true,
-        "End": true
-      };
-      if(stopkeys[key.name]) {
-        event.preventDefault();
-      }
-
-      if(key.numerical_code != 0) {
-        pressed_keys.push(key);
-      }
-    }, true);
-
-    wasm_instance = results.instance;
-    gamestate_ptr = results.instance.exports.initialise();
-
-    var previous_frame_timestamp = 0;
-
-    function update(timestamp) {
-      window.requestAnimationFrame(update);
-      let dt = timestamp - previous_frame_timestamp;
-      previous_frame_timestamp = timestamp;
-
-      for(let key of pressed_keys) {
-        wasm_instance.exports.key_pressed(
-          gamestate_ptr,
-          key.numerical_code,
-          key.ctrl, key.alt, key.shift);
-      }
-      pressed_keys = [];
-
-      results.instance.exports.update(gamestate_ptr, dt);
-    }
-    update(previous_frame_timestamp);
-
-  });
+    });
+}

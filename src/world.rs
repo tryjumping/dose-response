7c354117dfffa9bb650251e9696e8e0f08441842
Tomas Sedovic <tomas@sedovic.cz>
2018-02-04 14:23:59 +0100

    Fix world chunk seed generation
    All of our worlds terrains were identical, because we were multiplying
    the chunk's positions which were zero for the initial chunk.

--- src/world.rs
+++ src/world.rs
@@ -27,7 +27,9 @@ impl Chunk {
         // NOTE: `x` and `y` overflow on negative values here, but all
         // we care about is having a distinct value for each position
         // so our seeds don't repeat. So this is fine here.
-        let seed = Wrapping(world_seed as u64) * Wrapping(pos.x as u64) * Wrapping(pos.y as u64);
+        let seed = Wrapping(world_seed as u64) +
+            (Wrapping(13) * Wrapping(pos.x as u64)) +
+            (Wrapping(17) * Wrapping(pos.y as u64));
 
         // TODO: Monsters in different chunks will now have identical
         // IDs. We need to investigate whether that's a problem.

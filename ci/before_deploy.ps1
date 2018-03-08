51b67edb3eb1d5b435d01da0a3e91ff92a84e063
Tomas Sedovic <tomas@sedovic.cz>
2018-03-08 18:16:34 +0100

    Distribute readme and license with the binaries
    We'll need to trigger all the relevant builds to verify this.

--- ci/before_deploy.ps1
+++ ci/before_deploy.ps1
@@ -6,11 +6,16 @@ $STAGE = [System.Guid]::NewGuid().ToString()
 
 Set-Location $ENV:Temp
 New-Item -Type Directory -Name $STAGE
+New-Item -Type Directory -Name "$STAGE\Dose Response"
 Set-Location $STAGE
 
 $ZIP = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET).zip"
 
-Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\dose-response.exe" '.\'
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\dose-response.exe" '.\Dose Response\Dose Response.exe'
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\README.md" '.\Dose Response\README.txt'
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\COPYING.txt" '.\Dose Response\'
+Out-File -Path '.\Dose Response\VERSION.txt' -Value "Version: $($Env:APPVEYOR_REPO_TAG_NAME)"
+Add-Content -Path '.\Dose Response\VERSION.txt' -Value "Full Version: $($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET)"
 
 7z a "$ZIP" *
 
--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -19,7 +19,12 @@ main() {
 
     cross rustc --target $TARGET --release --no-default-features --features "opengl cli rand fullscreen" -- -C lto
 
-    cp target/$TARGET/release/dose-response $stage/
+    mkdir -p $stage/"Dose Response"
+    cp target/$TARGET/release/dose-response $stage/"Dose Response"
+    cp target/$TARGET/README.md $stage/"Dose Response"/README.txt
+    cp target/$TARGET/COPYING.txt $stage/"Dose Response"/
+    echo "Version: $TRAVIS_TAG" >> $stage/"Dose Response"/VERSION.txt
+    echo "Full Version: $CRATE_NAME-$TRAVIS_TAG-$TARGET" >> $stage/"Dose Response"/VERSION.txt
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *

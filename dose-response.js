68561377c9bc5a1624cc1c138481fdb6d54eda7d
Tomas Sedovic <tomas@sedovic.cz>
2018-05-04 13:34:42 +0200

    Clear webgl background to black

--- dose-response.js
+++ dose-response.js
@@ -70,7 +70,7 @@ function play_game(canvas, wasm_path) {
 
             twgl.resizeCanvasToDisplaySize(gl.canvas);
             gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-            gl.clearColor(1.0, 0.0, 1.0, 1.0);
+            gl.clearColor(0.0, 0.0, 0.0, 1.0);
             gl.clear(gl.COLOR_BUFFER_BIT);
 
             const uniforms = {

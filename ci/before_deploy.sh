b9f41472c6d95bf4e09df6634647cfe625d85042
Tomas Sedovic <tomas@sedovic.cz>
2018-05-09 21:06:19 +0200

    Set rpath from cargo config
    Cross does not pass the environment variables when it builds stuff.
    And it's cleaner to set the link args this way anyways.

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,7 +17,6 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    export LD_RUN_PATH='$ORIGIN/lib'
     cross rustc --target $TARGET --release --no-default-features --features "opengl sdl cli rand fullscreen" -- -C lto
 
     mkdir -p $stage/"Dose Response"

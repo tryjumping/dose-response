3a974d14d17ddfd756029866041bcafbaaaa4115
Tomas Sedovic <tomas@sedovic.cz>
2018-06-06 22:34:39 +0200

    Use 7zip to create the windows tarball
    Tar wasn't working right (couldn't figure out what was wrong from the
    appveyor log but maybe tar isn't actually on the system).
    
    Turns out, 7z is able to create a tarball, but it can only gz a single
    file, so you need to create it the old-fashioned way in two steps:
    first tar it up and then gzip it.

--- ci/before_deploy.ps1
+++ ci/before_deploy.ps1
@@ -10,7 +10,8 @@ New-Item -Type Directory -Name "$STAGE\Dose Response"
 Set-Location $STAGE
 
 #$ZIP = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET).zip"
-$TARBALL = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET).tar.gz"
+$TARFILE = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET).tar"
+$TARBALL = "$TARFILE.gz"
 
 Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\dose-response.exe" '.\Dose Response\Dose Response.exe'
 # NOTE(shadower): we're bundling things statically now, don't upload the full build directory anymore:
@@ -23,8 +24,9 @@ Add-Content -Path '.\Dose Response\VERSION.txt' -Value "Version: $($Env:APPVEYOR
 Add-Content -Path '.\Dose Response\VERSION.txt' -Value "Full Version: $($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET)"
 Add-Content -Path '.\Dose Response\VERSION.txt' -Value "Commit: $($Env:APPVEYOR_REPO_COMMIT)"
 
-# 7z a "$ZIP" *
-tar -czf "$TARBALL" *
+#7z a "$ZIP" *
+7z a "$TARFILE" *
+7z a "$TARBALL" "$TARFILE"
 
 # Push-AppveyorArtifact "$ZIP"
 Push-AppveyorArtifact "$TARBALL"

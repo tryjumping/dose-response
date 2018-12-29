e4c188e5b11096e4693d30024c2309745d690388
Tomas Sedovic <tomas@sedovic.cz>
2018-12-29 13:29:15 +0100

    Add settings and 1080p screenshots

--- src/windows/options.rs
+++ src/windows/options.rs
@@ -4,7 +4,7 @@ use crate::{
     point::Point,
     rect::Rectangle,
     state::State,
-    ui,
+    ui::{self, Button},
 };
 
 pub enum OptionItem {
@@ -15,12 +15,15 @@ pub enum OptionItem {
 struct Layout {
     window_rect: Rectangle,
     rect: Rectangle,
+    option_under_mouse: Option<OptionItem>,
+    rect_under_mouse: Option<Rectangle>,
+    fullscreen_button: Button,
 }
 
 pub struct Window;
 
 impl Window {
-    fn layout(&self, state: &State, _metrics: &dyn TextMetrics) -> Layout {
+    fn layout(&self, state: &State, metrics: &dyn TextMetrics) -> Layout {
         let screen_padding = Point::from_i32(2);
         let window_rect = Rectangle::from_point_and_size(
             screen_padding,
@@ -32,7 +35,24 @@ impl Window {
             window_rect.bottom_right() - (2, 1),
         );
 
-        Layout { window_rect, rect }
+        let mut option_under_mouse = None;
+        let mut rect_under_mouse = None;
+
+        let fullscreen_button = Button::new(rect.top_left(), "LOOOL").align_center(rect.width());
+
+        let button_rect = metrics.button_rect(&fullscreen_button);
+        if button_rect.contains(state.mouse.tile_pos) {
+            option_under_mouse = Some(OptionItem::Fullscreen);
+            rect_under_mouse = Some(button_rect);
+        }
+
+        Layout {
+            window_rect,
+            rect,
+            option_under_mouse,
+            rect_under_mouse,
+            fullscreen_button,
+        }
     }
 
     pub fn render(
@@ -54,7 +74,7 @@ impl Window {
         let sizes = sizes_str.join(" / ");
 
         let lines = vec![
-            Centered("Options"),
+            Centered("Settings"),
             Empty,
             Centered("Display:"),
             Centered("[F]ullscreen / [W]indow"),
@@ -62,8 +82,10 @@ impl Window {
             Centered(&font_size),
             Centered(&sizes),
             Empty,
-            Centered("Graphics backend (TODO):"),
+            Centered("Graphics backend:"),
             Centered("Glutin / SDL"),
+            Empty,
+            Centered("Back"),
         ];
 
         display.draw_rectangle(layout.window_rect, color::window_edge);
@@ -77,9 +99,16 @@ impl Window {
         );
 
         ui::render_text_flow(&lines, layout.rect, metrics, display);
+
+        if let Some(rect) = layout.rect_under_mouse {
+            display.draw_rectangle(rect, color::menu_highlight);
+        }
+
+        //display.draw_button(&layout.fullscreen_button);
+        let _ = layout.fullscreen_button;
     }
 
-    pub fn hovered(&self, _state: &State, _fwmetrics: &dyn TextMetrics) -> Option<OptionItem> {
-        None
+    pub fn hovered(&self, state: &State, metrics: &dyn TextMetrics) -> Option<OptionItem> {
+        self.layout(state, metrics).option_under_mouse
     }
 }

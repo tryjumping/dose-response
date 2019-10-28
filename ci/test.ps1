bc9eea18d146c1b07ebe6db5615c7324b0df860f
Tomas Sedovic <tomas@sedovic.cz>
2019-10-28 21:15:33 +0100

    another one
    This time the build actually finished, but the cargo call errored
    rigth from the start (even though it kept running)???

--- ci/test.ps1
+++ ci/test.ps1
@@ -3,16 +3,8 @@ If ($Env:APPVEYOR_REPO_TAG -eq "false") {
     # Print all environment variables
     gci env:* | sort-object name
 
-    cargo build --features "test windows-extra-features" --target $Env:TARGET
-    $native_call_success = $?
-    if (-not $native_call_success) {
-        throw 'error making native call'
-    }
-    cargo build --features "test windows-extra-features" --target $Env:TARGET --release
-    $native_call_success = $?
-    if (-not $native_call_success) {
-        throw 'error making native call'
-    }
+    cargo build --target "$Env:TARGET" --features "test windows-extra-features"
+    cargo build --target "$Env:TARGET" --features "test windows-extra-features" --release
 } Else {
-    Write-Host "This is a release tag: $Env:APPVEYOR_REPO_TAG. Skipping test builds."
+    Write-Host "This is a release tag: $Env:APPVEYOR_REPO_TAG_NAME. Skipping test builds."
 }

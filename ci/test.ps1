2a04372eb0d5ec8b813d6b04108cda516dc98326
Tomas Sedovic <tomas@sedovic.cz>
2019-10-28 20:37:10 +0100

    Fix (hopefully) PowerShell syntax errors

--- ci/test.ps1
+++ ci/test.ps1
@@ -1,10 +1,10 @@
-If ([%APPVEYOR_REPO_TAG%]==[false]) {
+If ([string]::IsNullOrEmpty($APPVEYOR_REPO_TAG)) {
     Write-Host "This is not a release tag. Running test builds."
     # Print all environment variables
     gci env:* | sort-object name
 
-    cargo build --features "test windows-extra-features" --target %TARGET% &&
-    cargo build --features "test windows-extra-features" --target %TARGET% --release
+    cargo build --features "test windows-extra-features" --target $TARGET
+    cargo build --features "test windows-extra-features" --target $TARGET --release
 } Else {
     Write-Host "This is a release tag: $APPVEYOR_REPO_TAG. Skipping test builds."
 }

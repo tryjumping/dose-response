a4e4498b2e4b481de98fcdc95554975e3b14ef4b
Tomas Sedovic <tomas@sedovic.cz>
2019-10-28 23:41:28 +0100

    Change the test script a little
    I'm not sure why this might help, but yeah Windows are broken again
    even though the build command actually seems to be working fine. IDK.

--- ci/test.ps1
+++ ci/test.ps1
@@ -3,13 +3,18 @@ If ($Env:APPVEYOR_REPO_TAG -eq "false") {
     # Print all environment variables
     gci env:* | sort-object name
 
-    cargo build --target "$($Env:TARGET)" --features "test"  2>&1 | %{ "$_" }
+    Write-Host "Compliling target: $Env:TARGET"
+
+    cargo build --target "$Env:TARGET" --features "test"  2>&1 | %{ "$_" }
     $native_call_success = $?
+    $native_call_success
     if (-not $native_call_success) {
         throw 'error'
     }
-    cargo build --target "$($Env:TARGET)" --features "test" --release  2>&1 | %{ "$_" }
+    cargo build --target "$Env:TARGET" --features "test" --release  2>&1 | %{ "$_" }
+
     $native_call_success = $?
+    $native_call_success
     if (-not $native_call_success) {
         throw 'error'
     }

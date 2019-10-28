b028b3bbd28938b32db30043fbbca9595650ce23
Tomas Sedovic <tomas@sedovic.cz>
2019-10-28 20:49:30 +0100

    Add powershell error handling
    The code above was not failing the build.

--- ci/test.ps1
+++ ci/test.ps1
@@ -1,10 +1,22 @@
-If ([string]::IsNullOrEmpty($APPVEYOR_REPO_TAG)) {
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction']='Stop'
+
+If ($Env:APPVEYOR_REPO_TAG == "false") {
     Write-Host "This is not a release tag. Running test builds."
     # Print all environment variables
     gci env:* | sort-object name
 
-    cargo build --features "test windows-extra-features" --target $TARGET
-    cargo build --features "test windows-extra-features" --target $TARGET --release
+    cargo build --features "test windows-extra-features" --target %TARGET%
+    $native_call_success = $?
+    if (-not $native_call_success) {
+        throw 'error making native call'
+    }
+    cargo build --features "test windows-extra-features" --target %TARGET% --release
+    $native_call_success = $?
+    if (-not $native_call_success) {
+        throw 'error making native call'
+    }
 } Else {
-    Write-Host "This is a release tag: $APPVEYOR_REPO_TAG. Skipping test builds."
+    Write-Host "This is a release tag: $Env:APPVEYOR_REPO_TAG. Skipping test builds."
 }

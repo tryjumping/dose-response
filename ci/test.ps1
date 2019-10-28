a814d20131b0d64433dbd06588d69f36d68e3412
Tomas Sedovic <tomas@sedovic.cz>
2019-10-28 23:02:42 +0100

    add err handling again

--- ci/test.ps1
+++ ci/test.ps1
@@ -4,7 +4,17 @@ If ($Env:APPVEYOR_REPO_TAG -eq "false") {
     gci env:* | sort-object name
 
     cargo build --target "$($Env:TARGET)" --features "test"  2>&1 | %{ "$_" }
+    $native_call_success = $?
+    $native_call_success
+    if (-not $native_call_success) {
+        throw 'error making native call'
+    }
     cargo build --target "$($Env:TARGET)" --features "test" --release  2>&1 | %{ "$_" }
+    $native_call_success = $?
+    $native_call_success
+    if (-not $native_call_success) {
+        throw 'error making native call'
+    }
 } Else {
     Write-Host "This is a release tag: $Env:APPVEYOR_REPO_TAG_NAME. Skipping test builds."
 }

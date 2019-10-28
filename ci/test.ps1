50e11e49d0b43ee3f21cce7805c635b615dcf88a
Tomas Sedovic <tomas@sedovic.cz>
2019-10-28 20:55:17 +0100

    Another windows test script fix
    String comparison is done with `-eq` not `==`.

--- ci/test.ps1
+++ ci/test.ps1
@@ -2,7 +2,7 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues['*:ErrorAction']='Stop'
 
-If ($Env:APPVEYOR_REPO_TAG == "false") {
+If ($Env:APPVEYOR_REPO_TAG -eq "false") {
     Write-Host "This is not a release tag. Running test builds."
     # Print all environment variables
     gci env:* | sort-object name

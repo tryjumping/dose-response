44c61deffe4c79a22050bb818e816d545522f706
Tomas Sedovic <tomas@sedovic.cz>
2019-10-28 21:25:04 +0100

    some more

--- ci/test.ps1
+++ ci/test.ps1
@@ -3,8 +3,13 @@ If ($Env:APPVEYOR_REPO_TAG -eq "false") {
     # Print all environment variables
     gci env:* | sort-object name
 
-    cargo build --target "$Env:TARGET" --features "test windows-extra-features"
-    cargo build --target "$Env:TARGET" --features "test windows-extra-features" --release
+    Write-Host "$($Env:TARGET)"
+    Write-Host "$Env:TARGET"
+    Write-Host "$TARGET"
+    Write-Host "%TARGET%"
+
+    cargo build --target "$($Env:TARGET)" --features "test windows-extra-features"
+    cargo build --target "$($Env:TARGET)" --features "test windows-extra-features" --release
 } Else {
     Write-Host "This is a release tag: $Env:APPVEYOR_REPO_TAG_NAME. Skipping test builds."
 }

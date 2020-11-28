e3be83c621bf9e71427a8f34934d79c87bbf9132
Tomas Sedovic <tomas@sedovic.cz>
2020-11-28 16:08:31 +0100

    Remove the wasm-gc calls
    According to its own website, it's no longer necessary. And indeed
    just doing a regular wasm build resulted in a smaller binary (596k)
    than what's currently live on the website (727k).

--- bin/wasm-release.sh
+++ bin/wasm-release.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-# conditionally run wasm gc
-
 WASM_BUILD_DIR=target/wasm32-unknown-unknown/release
-WASM_ORIGINAL_TARGET="$WASM_BUILD_DIR"/dose-response.wasm
-WASM_GC_TARGET="$WASM_BUILD_DIR"/dose-response-gc.wasm
+WASM_TARGET="$WASM_BUILD_DIR"/dose-response.wasm
 TARGET_WEB_DIR=target/web/
 
-if command -v wasm-gc >/dev/null 2>&1; then
-    echo Found wasm-gc
-    wasm-gc -o "$WASM_GC_TARGET" "$WASM_ORIGINAL_TARGET"
-else
-    echo No wasm-gc found, just copying the file over
-    cp "$WASM_ORIGINAL_TARGET" "$WASM_GC_TARGET"
-fi
-
 mkdir -p "$TARGET_WEB_DIR"
-cp "$WASM_GC_TARGET" "$TARGET_WEB_DIR"/dose-response.wasm
+cp "$WASM_TARGET" "$TARGET_WEB_DIR"/dose-response.wasm
 cp web-src/*.js "$TARGET_WEB_DIR"
 cp web-src/*.css "$TARGET_WEB_DIR"
 cp web-src/*.png "$TARGET_WEB_DIR"

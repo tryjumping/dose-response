15eee5287008852c9c2fb626010d456293d64090
Tomas Sedovic <tomas@sedovic.cz>
2021-04-05 10:11:52 +0200

    Initial sound effect support
    This adds three sinks specifically for sound effects. We need multiple
    sinks because a single Sink is only able to enqueue sounds, not mix
    them together. Which means the new sound only plays after the previous
    one has finished.
    
    Lots of improvements to do but this does actually mix sound effects
    together.

--- src/audio.rs
+++ src/audio.rs
@@ -3,14 +3,21 @@ use crate::random::Random;
 use rodio::{OutputStreamHandle, Sink};
 
 pub struct Audio {
-    pub background_sounds: BackgroundSounds,
-    pub background_queue: Sink,
+    pub backgrounds: BackgroundSounds,
+    pub background_sound_queue: Sink,
+    pub sound_effect_queue: [Sink; 3],
     pub rng: Random,
 }
 
 impl Audio {
     pub fn new(stream_handle: &OutputStreamHandle) -> Self {
-        let sink = Sink::try_new(&stream_handle).unwrap_or_else(|_| Sink::new_idle().0);
+        let background_sound_queue =
+            Sink::try_new(&stream_handle).unwrap_or_else(|_| Sink::new_idle().0);
+        let sound_effect_queue = [
+            Sink::try_new(&stream_handle).unwrap_or_else(|_| Sink::new_idle().0),
+            Sink::try_new(&stream_handle).unwrap_or_else(|_| Sink::new_idle().0),
+            Sink::try_new(&stream_handle).unwrap_or_else(|_| Sink::new_idle().0),
+        ];
 
         let forrest = {
             let bytes = include_bytes!("../assets/music/AMBForst_Forest (ID 0100)_BSB.ogg");
@@ -23,12 +30,38 @@ impl Audio {
         };
 
         Self {
-            background_sounds: BackgroundSounds {
+            backgrounds: BackgroundSounds {
                 ambient_forrest: forrest,
                 cow: cow,
             },
-            background_queue: sink,
+            background_sound_queue,
+            sound_effect_queue,
             rng: Random::new(),
+        }
+    }
+
+    pub fn play_sound_effect(&self, effect: Effect) {
+        // TODO: use the actual sound effects rather than hardcoding the backgrounds.cow sound
+        match rodio::Decoder::new(self.backgrounds.cow.clone()) {
+            Ok(sound) => {
+                // TODO Mix the sounds together. `queue.append` always waits for the sound to finish
+                // I *think* we'll need to add multiple sinks here and pick the one that's empty
+                if self.sound_effect_queue[0].empty() {
+                    self.sound_effect_queue[0].append(sound);
+                } else if self.sound_effect_queue[1].empty() {
+                    self.sound_effect_queue[1].append(sound);
+                } else if self.sound_effect_queue[2].empty() {
+                    self.sound_effect_queue[2].append(sound);
+                } else {
+                    log::warn!("play_sound_effect: no empty queue found. Skipping playback.");
+                }
+            }
+            Err(error) => {
+                log::error!(
+                    "play_sound_effect: Error decoding sound: {}. Skipping playback.",
+                    error
+                );
+            }
         }
     }
 }
@@ -53,4 +86,11 @@ impl BackgroundSounds {
             }
         }
     }
+}
+
+pub enum Effect {
+    Bump,
+    Move,
+    EatFood,
+    UseDose,
 }

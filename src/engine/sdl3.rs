e8bac6c98cc47c14ecf8f84696e463baf9ee6677
Tomas Sedovic <tomas@sedovic.cz>
2025-07-29 15:39:16 +0200

    Add an SDL3 example
    This shows the windows, reacts to the keyboard, runs an animation etc.
    
    Taken from the SDL3 docs: https://docs.rs/sdl3/0.14.36/sdl3/

--- src/engine/sdl3.rs
+++ src/engine/sdl3.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::unwrap_used)]
+
 use crate::{
     color::Color,
     engine::{
@@ -873,7 +875,47 @@ pub fn main_loop<S>(
 where
     S: SettingsStore + 'static,
 {
+    use sdl3::event::Event;
+    use sdl3::keyboard::Keycode;
+    use sdl3::pixels::Color;
+    use std::time::Duration;
+
     println!("Hello world from SDL3 backend!");
+    let sdl_context = sdl3::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("rust-sdl3 demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000_u32 / 60));
+    }
 
     Ok(())
 }

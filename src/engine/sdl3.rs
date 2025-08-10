dd5bfccd1ae85ae4706b3c5b6a8fccfe10059d5c
Tomas Sedovic <tomas@sedovic.cz>
2025-08-10 23:04:10 +0200

    Clean up SDL3 loop
    Remove a lot of comments and commented-out code. Pull the game
    updating/rendering code to a separate function. Remove some warnings.
    
    FIX the game update dt!!
    
    Turns out, we were doing integer conversion wrongly and the
    `target_dt` was always set to precisely `16ms`! That's why the always
    felt slightly off. Now that we're setting it correctly to
    `16.666...` (with whatever precision is available), the numbers make
    fare more sense.
    
    And I think this is pretty much it for the loop:
    
    We handle if the game update runs quickly (by having the explicit loop
    with small-increment waits to get to the next targeted time to
    execute).
    
    We don't wait if vsync or whatever handle timing for us -- we just hop
    on to the next update, no artificial waiting required.
    
    And if a frame takes longer, we'll again skip waiting and go directly
    to the next one. There shouldn't be any spiralling. We'll just drop
    FPS.

--- src/engine/sdl3.rs
+++ src/engine/sdl3.rs
@@ -3,13 +3,14 @@
 use crate::{
     color::Color,
     engine::{
-        self, Vertex,
+        self,
         loop_state::{self, LoopState, ResizeWindowAction, UpdateResult},
         opengl::OpenGlApp,
+        Vertex,
     },
     formula, keys,
     point::Point,
-    settings::{MIN_WINDOW_HEIGHT, MIN_WINDOW_WIDTH, Store as SettingsStore},
+    settings::{Store as SettingsStore, MIN_WINDOW_HEIGHT, MIN_WINDOW_WIDTH},
     state::State,
 };
 
@@ -19,7 +20,7 @@ use std::{
     time::{Duration, Instant},
 };
 
-use sdl3::{event::Event, keyboard::Keycode};
+use sdl3::{event::Event, keyboard::Keycode, render::Canvas, video::Window};
 
 use egui::{ClippedPrimitive, Context};
 
@@ -29,6 +30,32 @@ struct Game {
     cycle: u8,
     tick: u32,
     event_pump: sdl3::EventPump,
+}
+
+impl Game {
+    fn update_and_render(&mut self, dt: Duration, canvas: &mut Canvas<Window>) -> bool {
+        println!("Game update, {dt:?}");
+        self.tick += 1;
+
+        self.cycle = self.cycle.wrapping_add(1);
+        for event in self.event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => return false,
+                _ => {}
+            }
+        }
+
+        let i = self.cycle;
+        canvas.set_draw_color(sdl3::pixels::Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        canvas.present();
+
+        true
+    }
 }
 
 pub fn main_loop<S>(
@@ -68,7 +95,7 @@ where
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window("rust-sdl3 demo", 800, 600)
+        .window(window_title, 800, 600)
         .position_centered()
         .build()?;
 
@@ -95,7 +122,6 @@ where
     canvas.clear();
     canvas.present();
     let event_pump = sdl_context.event_pump()?;
-    let mut running = true;
 
     let mut game = Game {
         cycle: 0,
@@ -103,90 +129,50 @@ where
         event_pump,
     };
 
-    // NOTE: this gives us the more-or-less alternating 16/8/16/8ms cycles on MacOS
-    // Just like we had with winit+glutin.
-    // So that is something we want to handle. I don't want to run the update more than every 16ms.
-    // And I think that's probably the case to handle: the render loop can run faster than the updates and we need to throttle them.
+    let target_dt_nanoseconds = 1_000_000_000 / (formula::FPS as u32);
+    let target_dt = Duration::new(0, target_dt_nanoseconds);
+    dbg!(target_dt);
 
-    // NOTE: on Geralt there seems to be no vsync so we're getting like 300 micro(!!) second updates. So we will have to enable the vsync thing there. Well, this makes for good test cases.
-
+    // NOTE: this sets the boundary (variance) between actual `dt` and
+    // "fixed `dt`" based on the target FPS.
     //
-    // NOTE: I was seeing the exact behavior with the "fixed timestep"  algo, so that's not the full story without some sort of modification either
+    // 1ms variance is probably totally fine, actually, but if we need
+    // more precision, we can just supply a smaller number here.
+    let inc = Duration::new(0, 1_000_000); // 1ms
+    dbg!(inc);
 
-    // Wonder if we could split the looop into three parts:
-    // 1. poll events
-    // 2. is it time to update? then update
-    // 3. render (unconditionally)
-
-    // And actually, if polling the events faster doesn't matter (because we're not updating and it since update/render aren't decoupled now, things like animations or mouse can't be smoother), just incorporate events into game update:
-    // 1. is it time to update? then update
-    // 2. render (unconditionally)
-
-    // NOTE: this relies on vsync so we will have to put in some waiting to handle cases where vsync is off. I have a repro on Geralt for that right now.
-
-    // From: https://gafferongames.com/post/fix_your_timestep/
-    let target_dt = Duration::from_millis((1000.0 / formula::FPS) as u64);
-    let inc = Duration::from_millis(1);
-
-    //let mut total_elapsed_time = Duration::ZERO;
     let start_time = Instant::now();
     let mut current_time = start_time;
 
+    let mut running = true;
     while running {
         let elapsed_time = Instant::now().duration_since(start_time);
         let update_ready = (elapsed_time + inc) >= (game.tick * target_dt);
+
         if update_ready {
             let now = Instant::now();
             let dt = now.duration_since(current_time);
             current_time = now;
-            //total_elapsed_time += dt;
 
-            let game_update_start_time = Instant::now();
-            println!("Game update, {dt:?}");
-            game.tick += 1;
-
-            game.cycle = game.cycle.wrapping_add(1);
-            for event in game.event_pump.poll_iter() {
-                match event {
-                    Event::Quit { .. }
-                    | Event::KeyDown {
-                        keycode: Some(Keycode::Escape),
-                        ..
-                    } => running = false,
-                    _ => {}
-                }
-            }
-            log::info!(
-                "Game update duration: {:?}",
-                Instant::now().duration_since(game_update_start_time)
-            );
-
-            let render_start_time = Instant::now();
-            println!("Render");
-
-            let i = game.cycle;
-            canvas.set_draw_color(sdl3::pixels::Color::RGB(i, 64, 255 - i));
-            canvas.clear();
-            canvas.present();
-            log::info!(
-                "Render duration: {:?}",
-                Instant::now().duration_since(render_start_time)
-            );
+            running = game.update_and_render(dt, &mut canvas);
 
             let frame_dt = Instant::now().duration_since(current_time);
-            dbg!(frame_dt);
 
-            // Make sure we advance at least by `inc` time. Otherwise
+            // Make sure we advance at least by 1ms. Otherwise
             // we risk triggering update multiple times in a row if it
-            // takes less than `inc` time
-            if frame_dt < inc {
-                std::thread::sleep(inc);
+            // takes less than `inc` time.
+            let ms = Duration::from_millis(1);
+            debug_assert!(
+                inc <= ms,
+                "The frame catch-up increment must be smaller than 1ms"
+            );
+            if frame_dt < ms {
+                std::thread::sleep(ms);
             }
 
-            dbg!(
-                target_dt,
-                frame_dt,
-                (target_dt.as_secs_f64() - frame_dt.as_secs_f64()).abs()
+            log::info!(
+                "Total frame duration: {:?}",
+                Instant::now().duration_since(current_time)
             );
 
             // Expectation: dt ~ target_dt
@@ -195,94 +181,16 @@ where
                 target_dt * game.tick,
                 Instant::now().duration_since(start_time)
             );
-
-            log::info!(
-                "Total frame duration: {:?}",
-                Instant::now().duration_since(current_time)
-            );
         } else {
+            // Catch up to the next scheduled game update (based on
+            // `target_dt`) one increment at a time:
             std::thread::sleep(inc);
-            log::info!("Sleeping for: {inc:?}");
+
+            // NOTE: I found this to be much more precise, responsive
+            // (and more precisely controllable) than calculating for
+            // how long to sleep until the next frame starts.
         };
-        // TODO Let the game run and see if we're getting significant
-        // discrepancies (i.e. if the actual time is getting bigger
-        // and bigger than the expectation based on fixed dt.)
-        //
-        // I think a next step beyond a total sleep time would be to
-        // use an accumulator (with a sleep though! Maybe 1ms) to just
-        // catch up on any stragglers that way. Pretty much what fix
-        // your timestep does + the explicit wait.
-        //
-        // That way the discrepancy should never be more than 1ms
     }
-
-    // // From: https://gafferongames.com/post/fix_your_timestep/
-    // //let fixed_dt = Duration::from_millis((1000.0 / formula::FPS) as u64);
-    // let fixed_dt = Duration::from_millis((1000.0 / 120.0) as u64);
-
-    // let mut current_time = Instant::now();
-    // let mut accumulator = Duration::ZERO;
-
-    // let mut game_current_time = current_time;
-
-    // // TODO actually maybe just start with a naive loop and then see?
-    // // enable sleep for if we don't get vsync
-    // // and maybe detect if we're running more than 60 FPS?
-
-    // while running {
-    //     let new_time = Instant::now();
-    //     let frame_time = new_time - current_time;
-    //     current_time = new_time;
-
-    //     accumulator += frame_time;
-
-    //     while running && accumulator >= fixed_dt {
-    //         // TODO: I want a "real elapsed" DT here too that we ourselves calculate. Not just the const DT we specified above
-    //         {
-    //             let now = Instant::now();
-    //             let dt = now - game_current_time;
-    //             game_current_time = now;
-
-    //             game.tick += 1;
-    //             println!("Game update, {dt:?}ms");
-
-    //             game.cycle = game.cycle.wrapping_add(1);
-    //             for event in game.event_pump.poll_iter() {
-    //                 match event {
-    //                     Event::Quit { .. }
-    //                     | Event::KeyDown {
-    //                         keycode: Some(Keycode::Escape),
-    //                         ..
-    //                     } => running = false,
-    //                     _ => {}
-    //                 }
-    //             }
-    //             // The rest of the game loop goes here...
-    //         }
-
-    //         {
-    //             // simulate render loop taking 10 milliseconds
-    //             println!("Render");
-
-    //             let i = game.cycle;
-    //             canvas.set_draw_color(sdl3::pixels::Color::RGB(i, 64, 255 - i));
-    //             canvas.clear();
-    //             canvas.present();
-    //         }
-
-    //         // NOTE: this is problematic as it stands because it calculates the `dt` is fixed and irrespective of the actual time it took. I think at minimum we'd have to wait (if we're faster) to make sure we took at least dt times
-
-    //         // I think I need to actually calculate and test out the usecases: what if the elapsed time is shorter than dt? what if it's longer? what about rendering?
-    //         // NOTE: what motivated us to do this in the first place?
-    //         // first, super inconsistent dts on macos when waiting for render loops (16, 3, 16, 3, 16 ms IIRC)
-    //         // second, it'd be nicer for replays to have constant update framerate no matter what the display framerate did (e.g. always do 60fps update even on 120/144hZ displays)
-    //         // later on, would set us up for possibly smoothing animations out too but that's a distant thing concern
-
-    //         // SO first we really need to calculate proper elapsed dt and show it
-
-    //         accumulator -= fixed_dt;
-    //     }
-    // }
 
     Ok(())
 }

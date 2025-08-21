b4cdffd12f5fa221abd6b5b81a4a5e789ced1f47
Tomas Sedovic <tomas@sedovic.cz>
2025-08-21 08:32:31 +0200

    add notes on using opengl with sdl3

--- src/engine/sdl3.rs
+++ src/engine/sdl3.rs
@@ -129,6 +129,16 @@ where
         event_pump,
     };
 
+
+// NOTE: you should be able to use opengl here just like with SDL2 I think! This function still exists:
+// https://docs.rs/sdl3/latest/sdl3/video/struct.Window.html#method.gl_create_context
+
+// So look at the sdl2.rs code and see what that does. And then try to replicate that here.
+
+// https://docs.rs/sdl3/latest/sdl3/video/gl_attr/index.html
+
+
+
     let target_dt_nanoseconds = 1_000_000_000 / (formula::FPS as u32);
     let target_dt = Duration::new(0, target_dt_nanoseconds);
     dbg!(target_dt);
